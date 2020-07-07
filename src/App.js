c6a4244731e7ca0627637e29f98ee4af5c03b397
jinhyukoo <faill456@naver.com>
2020-07-07 20:24:38 +0900

    리액트로 만들어보는 전화번호부

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
   id = 2
@@ -15,7 +16,13 @@ class App extends Component {
         name: '이은민',
         phone: '010-4737-3702'
       }
-    ]
+    ],
+    keyword: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value,
+    });
   }
   handleCreate = (data) => {
     const { information } = this.state;
@@ -23,14 +30,45 @@ class App extends Component {
       information: information.concat({ id: this.id++, ...data })
     })
   }
-  render() {
+  handleRemove = (id) => {
     const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id
+        ? { ...info, ...data }
+        : info
+      )
+    })
+  }
+  render() {
+    const { information, keyword } = this.state;
+    const filteredList = information.filter(
+      info => info.name.indexOf(keyword) !== -1
+    );
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
         />
-        {JSON.stringify(information)}
+        <p>
+          <input
+            placeholder="검색 할 이름을 입력하세요.."
+            onChange={this.handleChange}
+            value={keyword}
+          />
+        </p>
+        <hr />
+        <PhoneInfoList
+          data={filteredList}
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
+        />
       </div>
     );
   }
